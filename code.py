c246206a35ba7d7b76a37460fa1afe0030677f02
Shriyamkgp <112169092+Shriyamkgp@users.noreply.github.com>
2025-04-17 01:44:46 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,9 @@
-!pip install -r requirements.txt
+import subprocess
+import sys
+
+# Install all packages listed in requirements.txt
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+
 import os
 import regex as re
 from scipy import signal
